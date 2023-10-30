860aa1d0cf888da1bf7759fd0e1d810aecd8836c
fabian <fasalpe34@gmail.ocm>
2023-10-30 13:41:21 -0500

    actualizacion

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const loadDb = require("../server/src/controllers/loadDb")
 
 conn.sync({ force: false }).then(() => {
 server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server Listening on port ${PORT}`);
   loadDb();
 })
 }).catch(error => console.error(error))

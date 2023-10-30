ee33193281b438612fa098f4e9fc21b0dd5ac2d3
fabian <fasalpe34@gmail.ocm>
2023-10-30 14:23:22 -0500

    "actualizacion 7"

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
 const PORT = process.env.PORT || 3001;
-const loadDb = require("../server/src/controllers/loadDb")
+
 
 conn.sync({ force: false }).then(() => {
 server.listen(PORT, () => {
